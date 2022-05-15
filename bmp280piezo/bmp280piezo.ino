491eecbb6f60a829a727757ae6eaa16df4f92dc4
danheeks@gmail.com <danheeks@gmail.com>
2022-05-15 17:01:10 +0100

    I made the climb rate go higher. I made the sink alarm less sensitive. I removed the serial prints

--- bmp280piezo/bmp280piezo.ino
+++ bmp280piezo/bmp280piezo.ino
@@ -21,8 +21,7 @@ float old_pressure = 0;
 
 void setup() {
 // play start sound
-//  Serial.begin(9600);
-   toneAC(388, 5);
+   toneAC(388, 4);
   delay(70);
   unsigned status;
   status = bmp.begin(0x76);
@@ -31,11 +30,16 @@ void setup() {
     toneAC(250, 10);
     delay(150);
     toneAC(0);
+    delay(150);
+    toneAC(250, 10);
+    delay(150);
+    toneAC(0);
+    delay(150);
+    toneAC(250, 10);
+    delay(150);
+    toneAC(0);
+    delay(150);
   }
-
-  toneAC(610, 3);
-  delay(70);
-  toneAC(0);
 
   /* Default settings from datasheet. */
   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
@@ -44,10 +48,12 @@ void setup() {
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_63); /* Standby time. */
 
-  old_pressure = bmp.readPressure();
+  sensors_event_t pressure_event;
+  bmp_pressure->getEvent(&pressure_event);
   
-  delay(70);
-  toneAC(610, 3);
+  old_pressure = pressure_event.pressure;
+  
+  toneAC(590, 4);
   delay(70);
   toneAC(0);
 }
@@ -59,20 +65,17 @@ void loop(){
   float new_pressure = pressure_event.pressure;
   float rate = (new_pressure - old_pressure) * 1000.0;
   old_pressure = new_pressure;
-  
-  //Serial.print(F("rate = "));
-  //Serial.println(rate);
 
   if(rate<-8)
   {
   float pitch = 500.0 - (rate * 10.0);
-  if(pitch > 1000)pitch = 1000.0;
+  if(pitch > 2000)pitch = 2000.0;
   toneAC((unsigned int)pitch);
   LowPower.idle(SLEEP_120MS, ADC_OFF, TIMER2_OFF, TIMER1_ON, TIMER0_OFF, SPI_OFF,USART0_OFF, TWI_OFF);
   toneAC(0);
   LowPower.idle(SLEEP_120MS, ADC_OFF, TIMER2_OFF, TIMER1_ON, TIMER0_OFF, SPI_OFF,USART0_OFF, TWI_OFF);
   }
-  else if(rate>20)
+  else if(rate>30)
   {
     toneAC(300, 1);
     LowPower.idle(SLEEP_250MS, ADC_OFF, TIMER2_OFF, TIMER1_ON, TIMER0_OFF, SPI_OFF,USART0_OFF, TWI_OFF);
