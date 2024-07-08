31a23d3703ac7c6fd5dff49ca8d834ad6191eeb3
danheeks <danheeks@gmail.com>
2024-07-08 21:50:27 +0100

    Added BMP280Tiny, plus latest other board

--- bmp280piezo/bmp280piezo.ino
+++ bmp280piezo/bmp280piezo.ino
@@ -20,11 +20,13 @@ D9 and D10 to the Piezo Buzzer ( see toneAC.h )
 float old_pressure = 0;
 
 void setup() {
+  Serial.begin(9600);
 // play start sound
    toneAC(388, 4);
   delay(70);
   unsigned status;
   status = bmp.begin(0x76);
+  Serial.println(status);
   if(!status)
   {
     toneAC(250, 10);
@@ -66,23 +68,29 @@ void loop(){
   float rate = (new_pressure - old_pressure) * 1000.0;
   old_pressure = new_pressure;
 
+  Serial.println((int)rate);
+
   if(rate<-8)
   {
   float pitch = 500.0 - (rate * 10.0);
   if(pitch > 2000)pitch = 2000.0;
   toneAC((unsigned int)pitch);
-  LowPower.idle(SLEEP_120MS, ADC_OFF, TIMER2_OFF, TIMER1_ON, TIMER0_OFF, SPI_OFF,USART0_OFF, TWI_OFF);
+//  LowPower.idle(SLEEP_120MS, ADC_OFF, TIMER2_OFF, TIMER1_ON, TIMER0_OFF, SPI_OFF,USART0_OFF, TWI_OFF);
+    delay(120);
   toneAC(0);
-  LowPower.idle(SLEEP_120MS, ADC_OFF, TIMER2_OFF, TIMER1_ON, TIMER0_OFF, SPI_OFF,USART0_OFF, TWI_OFF);
+//  LowPower.idle(SLEEP_120MS, ADC_OFF, TIMER2_OFF, TIMER1_ON, TIMER0_OFF, SPI_OFF,USART0_OFF, TWI_OFF);
+    delay(120);
   }
   else if(rate>30)
   {
     toneAC(300, 1);
-    LowPower.idle(SLEEP_250MS, ADC_OFF, TIMER2_OFF, TIMER1_ON, TIMER0_OFF, SPI_OFF,USART0_OFF, TWI_OFF);
+//    LowPower.idle(SLEEP_250MS, ADC_OFF, TIMER2_OFF, TIMER1_ON, TIMER0_OFF, SPI_OFF,USART0_OFF, TWI_OFF);
+    delay(250);
   }
   else
   {
     toneAC(0);
-    LowPower.idle(SLEEP_250MS, ADC_OFF, TIMER2_OFF, TIMER1_ON, TIMER0_OFF, SPI_OFF,USART0_OFF, TWI_OFF);
+    //LowPower.idle(SLEEP_250MS, ADC_OFF, TIMER2_OFF, TIMER1_ON, TIMER0_OFF, SPI_OFF,USART0_OFF, TWI_OFF);
+    delay(250);
   }
 }
