c45728a978a91d8b49b9ef7e90d43872cba8a3a4
danheeks <danheeks@gmail.com>
2024-10-16 14:44:53 +0100

    empty project added for the Atmel code for vario

---
+++ BMP280Tiny/AtmelCode/AtmelCode/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * AtmelCode.cpp
+ *
+ * Created: 16/10/2024 14:43:43
+ * Author : Admin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
